e64e3974a871bcbd18cd1760ac5e83695bed2232
Kyle Browning <kylebrowning@me.com>
2012-09-26 14:10:25 -0700

    Adds a loading indicator.

--- CustomStatusBar.h
+++ CustomStatusBar.h
@@ -5,12 +5,20 @@
 	UILabel* statusLabel;
 	/// Black View
   UIView *backgroundView;
+  UIImageView *loadingIndicator;
   BOOL shown;
   int statusbarHeight;
+  BOOL indicatorShown;
+  BOOL stopAnimating;
+  BOOL leftAnimationAdded;
+  BOOL rightAnimationAdded;
 }
-@property (nonatomic, retain) 	UILabel* statusLabel;
+@property (nonatomic, retain) UILabel* statusLabel;
+@property (nonatomic, assign) BOOL shown;
 -(void)showWithStatusMessage:(NSString*)msg;
+-(void)showWithStatusMessage:(NSString*)msg showLoadingIndicator:(BOOL)showLoadingIndicator;
 -(void)showWithStatusMessage:(NSString*)msg hide:(BOOL)shouldHide;
+-(void)showWithStatusMessage:(NSString*)msg hide:(BOOL)shouldHide showLoadingIndicator:(BOOL)showLoadingIndicator;
 -(void)hide;
 -(void)hide:(CGFloat)delay;
 -(void)hide:(CGFloat)delay withCompletition:(void (^)(BOOL finished))completition;
